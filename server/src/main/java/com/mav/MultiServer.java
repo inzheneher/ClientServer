14aa07bfc44bc3a02c8266f94eef11e3a629865b
inzheneher <inzheneher@ya.ru>
2016-08-02 22:51:31 +0700

    multi threading

--- server/src/main/java/com/mav/MultiServer.java
+++ server/src/main/java/com/mav/MultiServer.java
@@ -1,18 +1,37 @@
 package com.mav;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 class MultiServer extends Thread {
 
-    private Socket socket;
+    private Socket acceptSocket;
     private ObjectOutputStream outputStream;
     private ObjectInputStream inputStream;
     private LocalPrimeFactorizer primeFactorizer;
 
-    MultiServer(Socket socket) {
+    MultiServer(Socket acceptSocket) {
         super("MultiServer");
-        this.socket = socket;
+        this.acceptSocket = acceptSocket;
+    }
+
+    public void run(){
+
+        try {
+
+            primeFactorizer = new LocalPrimeFactorizer();
+
+            outputStream = new ObjectOutputStream(acceptSocket.getOutputStream());
+
+            inputStream = new ObjectInputStream(acceptSocket.getInputStream());
+
+            while (acceptSocket.isConnected()) {
+                outputStream.writeObject(primeFactorizer.factorize((Integer) inputStream.readObject()));
+            }
+        } catch (IOException |ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- server/src/main/java/com/mav/Server.java
+++ server/src/main/java/com/mav/Server.java
@@ -1,39 +1,27 @@
 package com.mav;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Server {
 
-    private ServerSocket serverSocket;
-    private Socket acceptSocket;
-    private ObjectOutputStream outputStream;
-    private ObjectInputStream inputStream;
-    private LocalPrimeFactorizer primeFactorizer;
-
     public static void main(String[] args) {
-        Server server = new Server();
-        server.run();
-    }
 
-        private void run(){
+        boolean listening = true;
+        ServerSocket serverSocket;
 
+        try {
+            serverSocket = new ServerSocket(12512);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        while (listening) {
             try {
-
-                serverSocket = new ServerSocket(12512);
-                acceptSocket = serverSocket.accept();
-                primeFactorizer = new LocalPrimeFactorizer();
-
-                outputStream = new ObjectOutputStream(acceptSocket.getOutputStream());
-
-                inputStream = new ObjectInputStream(acceptSocket.getInputStream());
-
-                while (acceptSocket.isConnected()) {
-                    outputStream.writeObject(primeFactorizer.factorize((Integer) inputStream.readObject()));
-                }
-            } catch (IOException |ClassNotFoundException e) {
+                new MultiServer(serverSocket.accept()).start();
+            } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
+    }
 }
